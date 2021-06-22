01ae26c12ef774b4b9432b9e5629985afbceab3c
Tanay Dilip Kewalramani <tanayke@cybage.com>
2021-06-22 09:45:35 +0530

    article card required data fetched and displayed

--- src/components/HomePage/YourFeedComponent.js
+++ src/components/HomePage/YourFeedComponent.js
@@ -1,52 +1,50 @@
-import React, { useEffect, useState } from 'react'
-import PropTypes from 'prop-types'
-import {Card,Container,Row,Col} from 'react-bootstrap';
-import { getAllArticles } from '../../services';
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import { Card, Container, Row, Col } from "react-bootstrap";
+import { getAllArticles } from "../../services";
 
-const YourFeedComponent = props => {
-    const[articles,setArticles]=useState([])
-    useEffect(()=>{
-        getAllArticles().then(data=>setArticles(data));
-    },[]);
-    return (
-        <>
-        <div className="p-3">
-       
-
-            {articles.map(article =>(
-              <Card key={article.id} style={{ width: '100%', margin:'1rem' }}>
-               
-                <Card.Body>
-      
-                    <Container>
-                        <Row>
-                            <Col md={7}>
-                            <Card.Text>Header</Card.Text>
-                            <Card.Title>{article.title}</Card.Title>
+const YourFeedComponent = (props) => {
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    getAllArticles().then((data) => setArticles(data));
+  }, []);
+  return (
+    <>
+      <div className="p-3">
+        {articles.map((article) => (
+          <Card key={article.id} style={{ width: "100%", margin: "1rem" }}>
+            <Card.Body>
+              <Container>
+                <Row>
+                  <Col md={7}>
                     <Card.Text>
-                  {article.description}
+                      {`${article.reporter.name} at ${article.location.locality}, ${article.location.city}`}{" "}
                     </Card.Text>
-                    <Card.Text className="text-muted">{article.uploadDateTime}</Card.Text>
-                            </Col>
-                            <Col md={5}>
-                            <Card.Img variant="top" src={`http://localhost:5500${ article.thumbnailImage }`} />
+                    <Card.Title>{article.title}</Card.Title>
+                    <Card.Text>{article.description}</Card.Text>
+                    <Card.Text className="text-muted">
+                      {article.uploadDateTime}
+                    </Card.Text>
+                  </Col>
+                  <Col md={5}>
+                    <Row>
+                      <Card.Img
+                        variant="top"
+                        src={`http://localhost:5500${article.thumbnailImage}`}
+                      />
+                    </Row>
+                    <Row>{article.category.name}</Row>
+                  </Col>
+                </Row>
+              </Container>
+            </Card.Body>
+          </Card>
+        ))}
+      </div>
+    </>
+  );
+};
 
-                            </Col>
-                        </Row>
-                    </Container>
-                  
-                </Card.Body>
-               
-              </Card>))}
-           
+YourFeedComponent.propTypes = {};
 
-        </div>
-        </>
-    )
-}
-
-YourFeedComponent.propTypes = {
-
-}
-
-export default YourFeedComponent
+export default YourFeedComponent;
