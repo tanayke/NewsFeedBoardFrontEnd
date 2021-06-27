7c6f25154a9828f471215100fbdd490158b1a6c7
Tanay Dilip Kewalramani <tanayke@cybage.com>
2021-06-28 02:05:07 +0530

    login register UI please use RWD

--- src/components/LandingPage/RegistrationPage/RegistrationPage.js
+++ src/components/LandingPage/RegistrationPage/RegistrationPage.js
@@ -132,46 +132,46 @@ export const RegistrationPage = () => {
         }) => (
           <div>
             <Row>
-              <Col sm={10}>
+              <Col md={8} className="ml-5">
                 <Form
                   ref={registrationForm}
                   onSubmit={handleSubmit}
-                  className='col-md-6'
+                  className="col-md-6"
                 >
-                  <h2 className='mt-5 mb-3'>
+                  <h2 className="mt-5 mb-3">
                     <span>REGISTRA</span>
                     <span style={{ color: "#007bff" }}>TION</span>
                   </h2>
 
                   <Form.Group
-                    name='name'
+                    name="name"
                     value={values.name}
                     onChange={handleChange}
                     onBlur={handleBlur}
-                    controlId='name'
+                    controlId="name"
                   >
                     <Form.Label>Full Name</Form.Label>
                     <Form.Control
-                      type='text'
-                      name='name'
-                      placeholder='Enter Your Full Name'
+                      type="text"
+                      name="name"
+                      placeholder="Enter Your Full Name"
                     />
                     {touched.name && errors.name && (
                       <div style={{ color: "red" }}>{errors.name}</div>
                     )}
                   </Form.Group>
                   <Form.Group
-                    name='email'
+                    name="email"
                     value={values.email}
                     onChange={handleChange}
                     onBlur={handleBlur}
-                    controlId='email'
+                    controlId="email"
                   >
                     <Form.Label>Email address</Form.Label>
                     <Form.Control
-                      type='email'
-                      placeholder='Enter Your Email'
-                      name='email'
+                      type="email"
+                      placeholder="Enter Your Email"
+                      name="email"
                     />
                     {/* <Form.Text className='text-muted'>
                   We will never share your email with anyone else.
@@ -181,39 +181,39 @@ export const RegistrationPage = () => {
                     )}
                   </Form.Group>
                   <Form.Group
-                    name='phone'
+                    name="phone"
                     value={values.phone}
                     onChange={handleChange}
                     onBlur={handleBlur}
-                    controlId='phone'
+                    controlId="phone"
                   >
                     <Form.Label>Phone</Form.Label>
                     <Form.Control
-                      type='phone'
-                      placeholder='Enter Mobile Number'
-                      name='phone'
+                      type="phone"
+                      placeholder="Enter Mobile Number"
+                      name="phone"
                     />
                     {/* <Form.Text className='text-muted'>
                   We will never share your Mobile Number with anyone else.
                 </Form.Text> */}
                     {touched.phone && errors.phone && (
-                      <div className='validation' style={{ color: "red" }}>
+                      <div className="validation" style={{ color: "red" }}>
                         {errors.phone}
                       </div>
                     )}
                   </Form.Group>
                   <Form.Group
-                    name='password'
+                    name="password"
                     value={values.password}
                     onChange={handleChange}
                     onBlur={handleBlur}
-                    controlId='password'
+                    controlId="password"
                   >
                     <Form.Label>Password</Form.Label>
                     <Form.Control
-                      type='password'
-                      placeholder='Enter Your Password'
-                      name='password'
+                      type="password"
+                      placeholder="Enter Your Password"
+                      name="password"
                     />
                     {touched.password && errors.password && (
                       <div style={{ color: "red" }}>{errors.password}</div>
@@ -221,17 +221,17 @@ export const RegistrationPage = () => {
                   </Form.Group>
 
                   <Form.Group
-                    name='confirmPassword'
+                    name="confirmPassword"
                     value={values.confirmPassword}
                     onChange={handleChange}
                     onBlur={handleBlur}
-                    controlId='confirmPassword'
+                    controlId="confirmPassword"
                   >
                     <Form.Label>Confirm Password</Form.Label>
                     <Form.Control
-                      type='password'
-                      placeholder='Enter Your Password Again'
-                      name='confirmPassword'
+                      type="password"
+                      placeholder="Enter Your Password Again"
+                      name="confirmPassword"
                     />
                     {touched.confirmPassword && errors.confirmPassword && (
                       <div style={{ color: "red" }}>
@@ -241,19 +241,19 @@ export const RegistrationPage = () => {
                   </Form.Group>
 
                   <Form.Group
-                    name='isReporter'
+                    name="isReporter"
                     value={values.isReporter}
                     onChange={handleChange}
                     onBlur={handleBlur}
-                    controlId='role'
+                    controlId="role"
                   >
                     <Form.Label>
                       Do you want to Post News Articles and Stories?{" "}
                     </Form.Label>
                     <Form.Check
-                      name='isReporter'
-                      type='checkbox'
-                      label='Check here'
+                      name="isReporter"
+                      type="checkbox"
+                      label="Check here"
                     />
                   </Form.Group>
 
@@ -275,25 +275,25 @@ export const RegistrationPage = () => {
                     <Button onClick={addNewLocation}> Add New Location</Button>
                   </Form.Group>
 
-                  <div className='mt-4'>
-                    <Button className='btn' variant='primary' type='submit'>
+                  <div className="mt-4">
+                    <Button className="btn" variant="primary" type="submit">
                       Register
                     </Button>
 
-                    <Link className='ml-3 mt-3' to={LOGIN}>
+                    <Link className="ml-3 mt-3" to={LOGIN}>
                       Already a User? Login
                     </Link>
                   </div>
                 </Form>
               </Col>
-              <Col sm={2} className='mt-5'>
+              <Col md={2} className="mt-5">
                 <Image
                   src={`${BASE_URL}/login.png`}
-                  widht={300}
-                  height={700}
-                  responsive
+                  widht="300vw"
+                  height="500vh"
                 />
               </Col>
+              <Col md={2} />
             </Row>
           </div>
         )}
