97c19d60a04863908ae88d0e3307a39073168113
tanayke <83388258+tanayke@users.noreply.github.com>
2021-07-05 10:16:13 +0530

    folder name lowercase

--- src/components/searchResultes/SearchResultsComponent.js
+++ src/components/searchResultes/SearchResultsComponent.js
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { ArticleFilterContext } from "../context/ArticleFilterContext/ArticleFilterContext";
-import { ArticleFeedComponent } from "../HomePage/ArticleFeeds/ArticleFeedComponent";
+import { ArticleFeedComponent } from "../homePage/ArticleFeeds/ArticleFeedComponent";
 
 export const SearchResultsComponent = () => {
   const { articleFilters } = useContext(ArticleFilterContext);
