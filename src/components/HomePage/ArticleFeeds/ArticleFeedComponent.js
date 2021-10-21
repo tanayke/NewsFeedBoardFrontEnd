57da0ee2ec34d0a7f775592e131d3fcddc01cb2a
Tanay Dilip Kewalramani <tanayke@cybage.com>
2021-10-21 09:59:01 +0530

    PaginationComponent changed

--- src/components/HomePage/ArticleFeeds/ArticleFeedComponent.js
+++ src/components/HomePage/ArticleFeeds/ArticleFeedComponent.js
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { ArticleFilterContext } from "../../context/ArticleFilterContext/ArticleFilterContext";
 import NewsCardsComponent from "../Utilities/NewsCardsComponent";
 import { getAllArticles } from "../../../services";
-import { PaginationComponent } from "../Utilities/PaginationComponent";
+import PaginationComponent from "../Utilities/PaginationComponent";
 
 const setOfContext = new Set();
 
@@ -10,35 +10,63 @@ const setOfContext = new Set();
 export const ArticleFeedComponent = () => {
   const [articleFeed, setArticleFeed] = useState([]);
   const { articleFilters } = useContext(ArticleFilterContext);
-  const { pageLimit, setPageLimit } = useState(0);
-  const fetchAndSetDataForPageChange = async (page) => {
+  const { categoryId, locationId, search, isTrending } = articleFilters;
+  const [pageLimit, setPageLimit] = useState(1);
+  const [currPage, setCurrPage] = useState(0);
+  // const fetchAndSetDataForPageChange = async (page) => {
+  //   try {
+  //     const data = await getAllArticles({ ...articleFilters, page });
+  //     setArticleFeed(data.items);
+  //     setPageLimit(data.totalPages);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+  setOfContext.add(articleFilters);
+  console.log(setOfContext);
+  useEffect(() => {
+    try {
+      console.log("currPage", currPage);
+      getAllArticles({ ...articleFilters, page: currPage }).then((data) => {
+        console.log(data);
+        setArticleFeed(data.items);
+        setPageLimit(data.totalPages);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, [categoryId, locationId, search, isTrending, currPage]);
+
+  useEffect(() => {
+    setCurrPage(0);
+
+    console.log("on articleFilter change", articleFeed);
+  }, [categoryId, locationId, search, isTrending]);
+
+  const afterPageClicked = async (page) => {
+    setCurrPage(page - 1);
+    console.log(currPage);
+
+    // eslint-disable-next-line no-param-reassign
+    page -= 1;
     try {
       const data = await getAllArticles({ ...articleFilters, page });
+      console.log(data);
       setArticleFeed(data.items);
       setPageLimit(data.totalPages);
     } catch (error) {
       console.log(error);
     }
   };
-  setOfContext.add(articleFilters);
-  console.log(setOfContext);
-  useEffect(() => {
-    getAllArticles(articleFilters).then((data) => {
-      setArticleFeed(data.items);
-      console.log(data);
-    });
-  }, [articleFilters]);
-
-  useEffect(() => {
-    console.log(articleFeed);
-  }, [articleFeed]);
-
   return (
     <div className="p-3">
       <NewsCardsComponent articleFeed={articleFeed} />
       <PaginationComponent
-        fetchAndSetDataForPageChange={fetchAndSetDataForPageChange}
-        pageLimit={pageLimit}
+        totPages={pageLimit}
+        currentPage={currPage + 1}
+        pageClicked={(ele) => {
+          afterPageClicked(ele);
+        }}
       />
     </div>
   );
--- src/components/HomePage/Utilities/PaginationComponent.js
+++ src/components/HomePage/Utilities/PaginationComponent.js
@@ -1,33 +1,180 @@
+/* eslint-disable react/no-array-index-key */
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React, { useContext, useEffect, useState } from "react";
-import { PageItem, Pagination } from "react-bootstrap";
-import { getAllArticles } from "../../../services";
-import { ArticleFilterContext } from "../../context/ArticleFilterContext/ArticleFilterContext";
 
-export const PaginationComponent = ({
-  fetchAndSetDataForPageChange,
-  pageLimit,
-}) => {
-  const [currentPage, setCurrentPage] = useState(0);
+import React from "react";
+import { Pagination } from "react-bootstrap";
 
-  const goToNextPage = () => {
-    if (currentPage !== pageLimit) setCurrentPage(currentPage + 1);
-  };
-  const goToPreviousPage = () => {
-    if (currentPage !== 0) setCurrentPage(currentPage - 1);
-  };
+export default function PaginationComponent(props) {
+  const [pageArray, setPageArray] = React.useState([]);
 
-  useEffect(() => {
-    fetchAndSetDataForPageChange(currentPage);
-  }, [currentPage]);
+  React.useEffect(() => {
+    const totPages = parseInt(props.totPages, 10);
+    const currentPage = parseInt(props.currentPage, 10);
+    let pageArr = [];
+    if (totPages > 1) {
+      if (totPages <= 9) {
+        let i = 1;
+        while (i <= totPages) {
+          pageArr.push(i);
+          i += 1;
+        }
+      } else if (currentPage <= 5)
+        pageArr = [1, 2, 3, 4, 5, 6, 7, 8, "", totPages];
+      else if (totPages - currentPage <= 4)
+        pageArr = [
+          1,
+          "",
+          totPages - 7,
+          totPages - 6,
+          totPages - 5,
+          totPages - 4,
+          totPages - 3,
+          totPages - 2,
+          totPages - 1,
+          totPages,
+        ];
+      else
+        pageArr = [
+          1,
+          "",
+          currentPage - 3,
+          currentPage - 2,
+          currentPage - 1,
+          currentPage,
+          currentPage + 1,
+          currentPage + 2,
+          currentPage + 3,
+          "",
+          totPages,
+        ];
+    }
+    setPageArray(pageArr);
+    // eslint-disable-next-line react/destructuring-assignment
+  }, [props.totPages]);
 
   return (
-    <div className="d-flex justify-content-center">
-      <Pagination>
-        <PageItem onClick={goToPreviousPage}> {`<`} </PageItem>
-        <PageItem onClick={goToNextPage}> {`>`} </PageItem>
-      </Pagination>
-    </div>
+    <>
+      {
+        // eslint-disable-next-line react/destructuring-assignment
+        props.children
+      }
+      <div className="d-flex justify-content-center">
+        <Pagination style={{ justifyContent: "center" }}>
+          {pageArray.map((ele, ind) => {
+            const toReturn = [];
+
+            if (ind === 0) {
+              toReturn.push(
+                <Pagination.First
+                  key="firstpage"
+                  onClick={
+                    props.currentPage === 1
+                      ? () => {}
+                      : () => {
+                          props.pageClicked(1);
+                        }
+                  }
+                />
+              );
+
+              toReturn.push(
+                <Pagination.Prev
+                  key="prevpage"
+                  onClick={
+                    props.currentPage === 1
+                      ? () => {}
+                      : () => {
+                          props.pageClicked(props.currentPage - 1);
+                        }
+                  }
+                />
+              );
+            }
+
+            if (ele === "") toReturn.push(<Pagination.Ellipsis key={ind} />);
+            else
+              toReturn.push(
+                <Pagination.Item
+                  key={ind}
+                  active={props.currentPage === ele}
+                  onClick={
+                    props.currentPage === ele
+                      ? () => {}
+                      : () => {
+                          props.pageClicked(ele);
+                        }
+                  }
+                >
+                  {ele}
+                </Pagination.Item>
+              );
+
+            if (ind === pageArray.length - 1) {
+              toReturn.push(
+                <Pagination.Next
+                  key="nextpage"
+                  onClick={
+                    props.currentPage === ele
+                      ? () => {}
+                      : () => {
+                          props.pageClicked(props.currentPage + 1);
+                        }
+                  }
+                />
+              );
+
+              toReturn.push(
+                <Pagination.Last
+                  key="lastpage"
+                  onClick={
+                    props.currentPage === ele
+                      ? () => {}
+                      : () => {
+                          props.pageClicked(ele);
+                        }
+                  }
+                />
+              );
+            }
+
+            return toReturn;
+          })}
+        </Pagination>
+      </div>
+    </>
   );
-};
+}
+// import React, { useContext, useEffect, useState } from "react";
+// import { PageItem, Pagination } from "react-bootstrap";
+// import { getAllArticles } from "../../../services";
+// import { ArticleFilterContext } from "../../context/ArticleFilterContext/ArticleFilterContext";
+
+// export const PaginationComponent = ({
+//   fetchAndSetDataForPageChange,
+//   pageLimit,
+// }) => {
+//   const [currentPage, setCurrentPage] = useState(0);
+
+//   const goToNextPage = () => {
+//     if (currentPage !== pageLimit) setCurrentPage(currentPage + 1);
+//   };
+//   const goToPreviousPage = () => {
+//     if (currentPage !== 0) setCurrentPage(currentPage - 1);
+//   };
+
+//   useEffect(() => {
+//     fetchAndSetDataForPageChange(currentPage);
+//   }, [currentPage]);
+
+//   return (
+//     <div className="d-flex justify-content-center">
+//       <Pagination>
+//         <PageItem onClick={goToPreviousPage}> {`<`} </PageItem>
+//         <PageItem onClick={goToNextPage}> {`>`} </PageItem>
+//       </Pagination>
+//     </div>
+//   );
+// };
+
+//* ************************************ */
